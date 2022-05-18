51f8c2ea31cf9b62ef78b1518b8ef7ba4e56d37d
“kangyu” <kangyu@huatu.com>
2022-05-18 08:38:30 +0800

    数组区间取5个不重复的值

---
+++ round-five-number.js
@@ -0,0 +1,26 @@
+const roundFiveNoRepeatNumber = function (min,max){
+    let result = [];
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    result.push(randomNumber);
+    while(result.length < 5){
+        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if(!result.includes(randomNumber)){
+            result.push(randomNumber);
+        }
+    }
+    return result;
+}
+
+const roundFiveNoRepeatNumber2 = function (max,min){
+    let enums = {}
+    let arr = Array.from({length:max-min},(v,k) => k)
+    let result = []
+    let x = 0
+    while (x < 5) {
+        let randomNumber = Math.floor(Math.random() * (max - min - x));
+        result.push(arr.splice(randomNumber,1))
+        x++
+    }
+    return result.flat()
+}
+console.log(roundFiveNoRepeatNumber2(99,88));
